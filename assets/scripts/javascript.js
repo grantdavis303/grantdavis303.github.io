c821c95694fed70ab0b31c50b3e0a16565bff879
Grant Davis <grantdavis303@gmail.com>
2025-04-16 12:28:29 -0600

    Update Formatted Time in JavaScript File

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -3,19 +3,27 @@ function updateTime() {
   var hourNow = today.getHours();
   var minuteNow = today.getMinutes();
 
+  // Format Hours
   if (hourNow > 12) {
     formattedHour = hourNow - 12;
-    am_pm = 'pm';
   } else {
     formattedHour = hourNow;
-    am_pm = 'am';
   }
 
+  // Format Minutes
   if (minuteNow >= 0 && minuteNow < 10) {
     formattedMinutes = '0' + minuteNow;
   } else {
     formattedMinutes = minuteNow;
   }
 
+  // Format AM or PM
+  if (hourNow >= 0 && hourNow < 12) {
+    am_pm = 'am'
+  } else {
+    am_pm = 'pm'
+  }
+
+  // Display Formatted Final Time
   document.getElementById("time").innerHTML = `${formattedHour}:${formattedMinutes} ${am_pm}`;
 }
